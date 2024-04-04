550543cd2967039e03dea047313b6719a47d4b60
Aakash Verma <dev.aakashv@gmail.com>
2024-04-05 02:40:45 +0530

    `update`: the files and resize the `Aakash` image

---
+++ src/components/socialMediaButton/socialMediaButton.ts
@@ -0,0 +1,3 @@
+export const SocialMediaButton = (text: string) => `
+    <a class="bg-gray_700 text-white font-bold text-sm" href="https://www.google.com/">${text}</a>
+`;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 import './style.css';
+import { SocialMediaButton } from './components/socialMediaButton/socialMediaButton';
+
+document.querySelector('#app')?.insertAdjacentHTML('afterbegin', SocialMediaButton('GitHub'));
