dbb19ac2478d658d94670b845b65f1b4ab357d1f
Aakash Verma <dev.aakashv@gmail.com>
2024-04-06 22:57:05 +0530

    `add`: the new elements to achieve the structure

--- src/components/userCard/userCard.ts
+++ src/components/userCard/userCard.ts
@@ -4,12 +4,16 @@ export const UserCard = (user: PersonDetailWithoutSocialLinks) => {
     const userImage = document.createElement('img');
     userImage.alt = user.name;
     userImage.title = user.name;
+    userImage.src = user.img;
     userImage.className = '';
     
     const userName = document.createElement('h1');
     userName.textContent = user.name;
     const userPlace = document.createElement('h2');
     userPlace.textContent = user.based_in;
+
+    const userDescription = document.createElement('p');
+    userDescription.textContent = `"${user.description}"`;
 
     const userPrimaryInformation = document.createElement('div');
     userPrimaryInformation.insertAdjacentElement('afterbegin', userName);
@@ -18,6 +22,6 @@ export const UserCard = (user: PersonDetailWithoutSocialLinks) => {
     const card = document.createElement('div');
     card.insertAdjacentElement('afterbegin', userImage);
     card.insertAdjacentElement('beforeend', userPrimaryInformation);
-    console.log(card)
+    card.insertAdjacentElement('beforeend', userDescription);
     return card;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
+import { SocialMediaButton } from './components/socialMediaButton/socialMediaButton';
 import SocialMediaCard from './components/socialMediaCard/socialMediaCard';
 import { UserCard } from './components/userCard/userCard';
 import socialMediaData from './data/social-media/socialMediaData';
 import './style.css';
-import { PersonDetailWithoutSocialLinks } from './types/socialMedia.types';
-
-// document.querySelector('#app')?.insertAdjacentElement('afterbegin', SocialMediaButton({
-//     text: 'Github',
-//     link: 'https://www.github.com',
-// } as SocialMediaLink));
+import { PersonDetailWithoutSocialLinks, SocialMediaLink } from './types/socialMedia.types';
 
 const { socialLinks, ...personWithoutSocialLinks } = socialMediaData()[0];
+const mainCard = SocialMediaCard(UserCard(personWithoutSocialLinks as PersonDetailWithoutSocialLinks)) as Element;
+const linkContainer = document.createElement('div');
+
+socialLinks.map(({ text, link }: SocialMediaLink) => {
+    const button = SocialMediaButton({ text, link } as SocialMediaLink); // Create the button
+    linkContainer.insertAdjacentElement('beforeend', button); // Append each button
+});
+
+mainCard.insertAdjacentElement('beforeend', linkContainer);
 
 document.querySelector('#app')?.insertAdjacentElement(
     'afterbegin',
-    SocialMediaCard(UserCard(personWithoutSocialLinks as PersonDetailWithoutSocialLinks)) as Element
+    mainCard
 );
